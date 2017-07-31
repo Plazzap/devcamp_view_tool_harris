954bc58fb9a3ddcdb1600f2a5152762587ab3ef2
Carter Harris <carter.kc.harris@gmail.com>
2017-07-31 15:50:52 +0000

    Devtool initial commit

---
+++ lib/devcamp_view_tool_harris.rb
@@ -0,0 +1,6 @@
+require "devcamp_view_tool_harris/version"
+require "devcamp_view_tool_harris/renderer"
+
+module DevcampViewToolHarris
+  # Your code goes here...
+end
---
+++ spec/devcamp_view_tool_harris_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+describe DevcampViewToolHarris do
+  it "has a version number" do
+    expect(DevcampViewToolHarris::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
